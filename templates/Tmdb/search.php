c6d8499af41f5c0d54da68fd6d8019576b833073
Rafael Popescu <rafael.popescu140@gmail.com>
2023-07-08 17:21:35 +0300

    feat(pagination): Show total result in pagination

--- templates/Tmdb/search.php
+++ templates/Tmdb/search.php
@@ -61,43 +61,44 @@ $this->Breadcrumbs->add(__d('movies', 'TMDB'), null, ['class' => 'breadcrumb-ite
             <?php endif; ?>
         </div>
         <!--end::Row-->
-        <!--begin::Pagination-->
-        <div class="d-flex flex-stack flex-wrap pt-10">
-            <div class="fs-6 fw-bold text-gray-700">Showing 1 to 10 of 50 entries</div>
-            <!--begin::Pages-->
-            <ul class="pagination">
-                <li class="page-item previous">
-                    <a href="#" class="page-link">
-                        <i class="previous"></i>
-                    </a>
-                </li>
-                <li class="page-item active">
-                    <a href="#" class="page-link">1</a>
-                </li>
-                <li class="page-item">
-                    <a href="#" class="page-link">2</a>
-                </li>
-                <li class="page-item">
-                    <a href="#" class="page-link">3</a>
-                </li>
-                <li class="page-item">
-                    <a href="#" class="page-link">4</a>
-                </li>
-                <li class="page-item">
-                    <a href="#" class="page-link">5</a>
-                </li>
-                <li class="page-item">
-                    <a href="#" class="page-link">6</a>
-                </li>
-                <li class="page-item next">
-                    <a href="#" class="page-link">
-                        <i class="next"></i>
-                    </a>
-                </li>
-            </ul>
-            <!--end::Pages-->
-        </div>
-        <!--end::Pagination-->
+        <?php if (isset($search_term)): ?>
+            <!--begin::Pagination-->
+            <div class="d-flex flex-stack flex-wrap pt-10">
+                <div class="fs-6 fw-bold text-gray-700">Showing 1 to 20 of <?= $tmdb_result['total_results'] ?> entries</div>                
+                <ul class="pagination">                    
+                    <li class="page-item previous">
+                        <a href="#" class="page-link">
+                            <i class="previous"></i>
+                        </a>
+                    </li>
+                    <li class="page-item active">
+                        <a href="#" class="page-link">1</a>
+                    </li>
+                    <li class="page-item">
+                        <a href="#" class="page-link">2</a>
+                    </li>
+                    <li class="page-item">
+                        <a href="#" class="page-link">3</a>
+                    </li>
+                    <li class="page-item">
+                        <a href="#" class="page-link">4</a>
+                    </li>
+                    <li class="page-item">
+                        <a href="#" class="page-link">5</a>
+                    </li>
+                    <li class="page-item">
+                        <a href="#" class="page-link">6</a>
+                    </li>
+                    <li class="page-item next">
+                        <a href="#" class="page-link">
+                            <i class="next"></i>
+                        </a>
+                    </li>
+                </ul>
+                <!--end::Pages-->
+            </div>
+            <!--end::Pagination-->
+        <?php endif; ?>
     </div>
     <!--end::Tab pane-->
 </div>
